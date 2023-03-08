c0ece5be6836ff5990fb0045ce93fcb7f9228b81
yanis <yanis@gmail.com>
2023-03-08 02:29:53 +0100

    premier commite

---
+++ src/components/formulair.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleInputChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...formData }),
+    })
+      .then(() => alert('Success!'))
+      .catch((error) => alert(error));
+  };
+
+  const encode = (data) => {
+    return Object.keys(data)
+      .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="hidden" name="form-name" value="contact" />
+      <div>
+        <label htmlFor="name">Nom :</label>
+        <input type="text" name="name" id="name" onChange={handleInputChange} />
+      </div>
+      <div>
+        <label htmlFor="email">Email :</label>
+        <input type="email" name="email" id="email" onChange={handleInputChange} />
+      </div>
+      <div>
+        <label htmlFor="message">Message :</label>
+        <textarea name="message" id="message" onChange={handleInputChange} />
+      </div>
+      <button type="submit">Envoyer</button>
+    </form>
+  );
+};
+
+export default ContactForm;
