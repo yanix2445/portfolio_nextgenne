cfce248efd521ad79dce0386e358bb0e0912807c
yanis <yanis@gmail.com>
2023-03-08 03:30:50 +0100

    popu-up 2

--- src/components/formulair.tsx
+++ src/components/formulair.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
@@ -6,8 +8,6 @@ const ContactForm = () => {
     email: '',
     message: '',
   });
-
-  const [showModal, setShowModal] = useState(false);
 
   const handleInputChange = (event: any) => {
     setFormData({ ...formData, [event.target.name]: event.target.value });
@@ -21,10 +21,9 @@ const ContactForm = () => {
       body: encode({ 'form-name': 'contact', ...formData }),
     })
       .then(() => {
-        setShowModal(true);
-        setTimeout(() => {
-          setShowModal(false);
-        }, 2000);
+        toast.success('Message envoyé avec succès !', {
+          position: toast.POSITION.BOTTOM_RIGHT,
+        });
       })
       .catch((error) => alert(error));
   };
@@ -36,22 +35,25 @@ const ContactForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="hidden" name="form-name" value="contact" />
-      <div>
-        <label htmlFor="name">Nom :</label>
-        <input type="text" name="name" id="name" onChange={handleInputChange} />
-      </div>
-      <div>
-        <label htmlFor="email">Email :</label>
-        <input type="email" name="email" id="email" onChange={handleInputChange} />
-      </div>
-      <div>
-        <label htmlFor="message">Message :</label>
-        <textarea name="message" id="message" onChange={handleInputChange} />
-      </div>
-      <button type="submit">Envoyer</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="hidden" name="form-name" value="contact" />
+        <div>
+          <label htmlFor="name">Nom :</label>
+          <input type="text" name="name" id="name" onChange={handleInputChange} />
+        </div>
+        <div>
+          <label htmlFor="email">Email :</label>
+          <input type="email" name="email" id="email" onChange={handleInputChange} />
+        </div>
+        <div>
+          <label htmlFor="message">Message :</label>
+          <textarea name="message" id="message" onChange={handleInputChange} />
+        </div>
+        <button type="submit">Envoyer</button>
+      </form>
+      <ToastContainer />
+    </>
   );
 };
 
